6f200b99b7e232285f3643742723d71ec835c128
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-06 15:12:53 +0900

    파일 목록

---
+++ src/Collection/FileExercise.java
@@ -0,0 +1,18 @@
+package Collection;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileExercise {
+    public static void main(String[] args) throws FileNotFoundException {
+        // ./
+//        FileReader fileReader = new FileReader("./aa_file.txt");
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        for(File file : files) {
+            System.out.println(file);
+        }
+//        System.out.println(files[0]);
+    }
+}
