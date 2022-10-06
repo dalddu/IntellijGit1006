5920a5f5f63a6e057a5a24634b1867095c77769f
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-06 10:45:48 +0900

    RndNumbersWithoutDuplicated

---
+++ src/collection01/RndNumbersWithoutDuplicated.java
@@ -0,0 +1,19 @@
+package Collection01;
+
+import java.util.HashSet;
+
+public class RndNumbersWithoutDuplicated {
+    public static void main(String[] args) {
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        HashSet<Integer> numbers = new HashSet<>();
+
+        for (int i=0; i<200; i++) {
+            int r = randomNumberGenerator.generate(20);
+            numbers.add(r);
+
+                }
+        System.out.println(numbers);
+        System.out.println(numbers.size());
+
+    }
+}
