821064912779026a6149307041be8d8ed0f78bd3
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-06 10:15:56 +0900

    RndNumbersWithoutDuplicated

---
+++ src/Collection/RndNumbersWithoutDuplicated.java
@@ -0,0 +1,12 @@
+package Collection;
+
+public class RndNumbersWithoutDuplicated {
+    public static void main(String[] args) {
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+                for (int i=0; i<50; i++) {
+                   int r = randomNumberGenerator.generate(10);
+                    System.out.println(r);
+                }
+
+    }
+}
