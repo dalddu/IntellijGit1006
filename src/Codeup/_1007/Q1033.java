29ed22f1e4dcd9b88d7c7c375fb617012b9c5ce7
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-10 21:06:59 +0900

    코드업 1033

---
+++ src/Codeup/_1007/Q1033.java
@@ -0,0 +1,13 @@
+package Codeup._1007;
+import java.util.Scanner;
+
+public class Q1033 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.printf("%X", a);
+
+    }
+
+}
