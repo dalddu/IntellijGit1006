7583df8e242d01e573be908a67a956a8acf5e55f
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-10 21:06:46 +0900

    코드업 1032

---
+++ src/Codeup/_1007/Q1032.java
@@ -0,0 +1,11 @@
+package Codeup._1007;
+
+import java.util.Scanner;
+
+public class Q1032 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.printf("%x", a);
+    }
+}
