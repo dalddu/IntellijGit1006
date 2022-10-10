21058781370ec849807c880389faa852639144f5
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-10 21:00:32 +0900

    코드업 1012

---
+++ src/Codeup/_1005/Q1012.java
@@ -0,0 +1,10 @@
+package Codeup._1005;
+import java.util.Scanner;
+
+public class Q1012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        System.out.println(a);
+    }
+}
