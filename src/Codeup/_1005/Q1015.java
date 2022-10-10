87d557d70a2d2c5b192e9768bb707ba2bbe0d7fe
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-10 21:01:18 +0900

    코드업 1015

---
+++ src/Codeup/_1005/Q1015.java
@@ -0,0 +1,11 @@
+package Codeup._1005;
+
+import java.util.Scanner;
+
+public class Q1015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        System.out.printf("%.2f",a);
+    }
+}
