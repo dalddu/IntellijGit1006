16963d8140afe0a27fe62bbfb10bafe32b847934
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-10 20:59:43 +0900

    코드업 1011

---
+++ src/Codeup/_1005/Q1011.java
@@ -0,0 +1,11 @@
+package Codeup._1005;
+
+import java.util.Scanner;
+
+public class Q1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        System.out.println(a);
+    }
+}
