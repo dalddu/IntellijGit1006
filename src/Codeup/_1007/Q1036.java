36f050cccc2d9a7c43fa0151dc2fd874461246b5
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-10 21:07:41 +0900

    코드업 1036

---
+++ src/Codeup/_1007/Q1036.java
@@ -0,0 +1,12 @@
+package Codeup._1007;
+
+import java.util.Scanner;
+
+public class Q1036 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        int b = (int)a;
+        System.out.println(b);
+    }
+}
