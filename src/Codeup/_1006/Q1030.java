aaeb200bf6a21072d4eb3d993dcbf077d7f1acdd
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-10 21:06:21 +0900

    코드업 1030

---
+++ src/Codeup/_1006/Q1030.java
@@ -0,0 +1,11 @@
+package Codeup._1006;
+
+import java.util.Scanner;
+
+public class Q1030 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long i = sc.nextLong();
+        System.out.println(i);
+    }
+}
