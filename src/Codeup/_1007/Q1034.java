7336fcadfc5587933d65d546b845f8ff378bc388
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-10 21:07:12 +0900

    코드업 1034

---
+++ src/Codeup/_1007/Q1034.java
@@ -0,0 +1,11 @@
+package Codeup._1007;
+
+import java.util.Scanner;
+
+public class Q1034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(8);
+        System.out.printf("%d", a);
+    }
+}
