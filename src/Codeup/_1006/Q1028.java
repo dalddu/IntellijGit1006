286212815033dd6846f5401ed9bc05eaa2b9321c
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-10 21:05:47 +0900

    코드업 1028

---
+++ src/Codeup/_1006/Q1028.java
@@ -0,0 +1,12 @@
+package Codeup._1006;
+
+import java.util.Scanner;
+
+public class Q1028 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long i = sc.nextLong();
+        System.out.println(i);
+    }
+}
+
