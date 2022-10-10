3f6ef99388a5fb505e64eb75fb196aa015f5f8bb
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-10 21:06:02 +0900

    코드업 1029

---
+++ src/Codeup/_1006/Q1029.java
@@ -0,0 +1,12 @@
+package Codeup._1006;
+
+import java.util.Scanner;
+
+public class Q1029 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double i = sc.nextDouble();
+        System.out.printf("%.11f",i);
+    }
+}
+
