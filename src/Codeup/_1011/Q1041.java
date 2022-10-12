5159637bf03780e74bf82c859dcbde95d4510e3e
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-13 00:34:20 +0900

    코드업 1041

---
+++ src/Codeup/_1011/Q1041.java
@@ -0,0 +1,11 @@
+package Codeup._1011;
+
+import java.util.Scanner;
+
+public class Q1041 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        System.out.printf("%c", a+1);
+    }
+}
