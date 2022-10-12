a5a223f9a8d9780b2d3403fadde6c9c09d3b8a2c
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-13 00:32:50 +0900

    코드업 1056

---
+++ src/Codeup/_1012/Q1056.java
@@ -0,0 +1,16 @@
+package Codeup._1012;
+
+import java.util.Scanner;
+
+public class Q1056 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a!=b) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
