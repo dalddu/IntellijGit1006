83312465022ddc63e4ef571154fce826f35eb65c
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-13 00:31:16 +0900

    코드업 1053

---
+++ src/Codeup/_1012/Q1053.java
@@ -0,0 +1,11 @@
+package Codeup._1012;
+
+import java.util.Scanner;
+
+public class Q1053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a == 0 ? 1 : 0);
+    }
+}
