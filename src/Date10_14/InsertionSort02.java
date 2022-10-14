82d719ffeea509e2b17c911a6170543c91ddbf32
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-14 10:50:49 +0900

    InsertionSort2 루프 i를 적용

--- src/Date10_14/InsertionSort02.java
+++ src/Date10_14/InsertionSort02.java
@@ -3,24 +3,18 @@ package Date10_14;
 import java.util.Arrays;
 
 public class InsertionSort02 {
-    //재귀(recursive) -- 반복문(while)없이 반복해야 할 때
+    // 루프 i를 적용
     public int[] sort(int[] arr) {
-        // i=1 arr[1] vs arr[0]
-        // i=2 arr[2] vs arr[1]
-        // i=2 arr[1] vs arr[0]
-        for(int i=1; i<arr.length-1; i++) {
-            for(int j=i; j>0; j--) {
-            if (arr[j] < arr[j - 1]) { // arr[1] vs arr[0]
-                int temp = arr[j-1];
-                arr[j-1] = arr[j];
-                arr[j] = temp;
-                /*
-                System.out.printf("i:%d j:%d j-1:%d\n", i, j, j-1) // 재귀
-                 */
+        for(int i=1; i<arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) { // arr[1] vs arr[0]
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
             }
         }
         return arr;
-        // return sort(arr, i+1) 재귀는  자기자신을 빼고 돌려야함
     }
 
     public static void main(String[] args) {
@@ -33,4 +27,3 @@ public class InsertionSort02 {
         System.out.println(Arrays.toString(result));
         }
     }
-}
