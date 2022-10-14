4276eed264126ade6f4328333f5d0f074d81999b
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-14 10:08:40 +0900

    InsertionSort

--- src/Date10_14/InsertionSort01.java
+++ src/Date10_14/InsertionSort01.java
@@ -6,11 +6,11 @@ public class InsertionSort01 {
 
     public int[] sort(int[] arr) {
         int i = 1;
-        if (arr[i] < arr[i - 1]) // arr[1] vs arr[0]
-        //swqp
-
+        if (arr[i] < arr[i - 1]) { // arr[1] vs arr[0]
+            int temp = arr[i-1];
+            arr[i-1] = arr[i];
+            arr[i] = temp;
     }
-
 
         return new int[]{};
     }
@@ -19,6 +19,7 @@ public class InsertionSort01 {
         // 5에서 출발을 합니다  idx = 1
         int[] arr = {8, 5, 6, 2, 4};
         InsertionSort01 is = new InsertionSort01();
+        arr = new int[] {5, 8, 6, 2, 4};
         int[] result = is.sort(arr);
 
         System.out.println(Arrays.toString(result));
