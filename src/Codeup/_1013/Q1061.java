88fd9bcf8f6bf3f814ca9ec65543fc7189a81ea7
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-18 00:45:27 +0900

    코드업 1061

---
+++ src/Codeup/_1013/Q1061.java
@@ -0,0 +1,12 @@
+package Codeup._1013;
+
+import java.util.Scanner;
+
+public class Q1061 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a|b);
+    }
+}
