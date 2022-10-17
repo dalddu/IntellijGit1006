624e296ebe20728ef7d08fe8842a141539a82b3b
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-18 00:50:35 +0900

    코드업 1079

---
+++ src/Codeup/_1014/Q1079.java
@@ -0,0 +1,18 @@
+package Codeup._1014;
+
+import java.util.Scanner;
+
+public class Q1079 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            char a = sc.next().charAt(0);
+            if(a=='q') {
+                System.out.println(a);
+                break;
+            }else {
+                System.out.println(a);
+            }
+        }
+    }
+}
