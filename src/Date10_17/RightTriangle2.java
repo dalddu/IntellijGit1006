9f84ae396cca58222095681a458cb2addb4700e9
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-17 09:24:12 +0900

    RightTriangle2

--- src/Date10_17/RightTriangle2.java
+++ src/Date10_17/RightTriangle2.java
@@ -9,15 +9,15 @@ public class RightTriangle2 {
         * * *
         * * * *
          */
-        int num = 0;
+        System.out.println("라인 수를 입력하세요>> ");
         Scanner sc = new Scanner(System.in);
-        System.out.print("라인 수를 입력하세요>> ");
-        String tmp = sc.nextLine();
-        for(int i=1; i<=5; i++){
-            for(int j=1; j<=i; j++){
-                System.out.println("*");
-            }
-        }   System.out.println();
+        int num = sc.nextInt();
 
+        for(int i=1; i<=num; i++) {
+            for(int j=1; j<=i;  j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
