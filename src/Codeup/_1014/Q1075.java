e613a17fcf7b184cd6c12ca73f5c68160a511273
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-18 00:49:54 +0900

    코드업 1075

---
+++ src/Codeup/_1014/Q1075.java
@@ -0,0 +1,14 @@
+package Codeup._1014;
+
+import java.util.Scanner;
+
+public class Q1075 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        while (a>0) {
+            System.out.println(a-1);
+            a--;
+        }
+    }
+}
