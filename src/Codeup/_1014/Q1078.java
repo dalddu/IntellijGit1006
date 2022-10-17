b41f84cb5b5a72f1cd6a2b265a37ebceb6609502
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-18 00:50:24 +0900

    코드업 1078

---
+++ src/Codeup/_1014/Q1078.java
@@ -0,0 +1,17 @@
+package Codeup._1014;
+
+import java.util.Scanner;
+
+public class Q1078 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int sum = 0;
+        for(int i=0; i<=a; i++){
+            if(i%2==0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
